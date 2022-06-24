52b097b1f19a35b25a04b6fc4dc2c11462f64ec1
Scott Berry <berryscottr@gmail.com>
2022-06-24 18:53:35 -0400

    refactor workflow envs

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 
 func main() {
 	gobot := bot.Data{Token: os.Getenv("BOT_TOKEN")}
+	if gobot.Token == "" {
+		panic("BOT_TOKEN not set")
+	}
 	gobot.Start()
 	<-make(chan struct{})
+	return
 }
