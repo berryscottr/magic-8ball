551ce8d32b6d4ed8f3b28dd6837379b49c54349e
Scott Berry <berryscottr@gmail.com>
2023-04-10 14:13:23 -0400

    spring 23 week 13 inputs

--- scripts/slmatchups/eight_ball.py
+++ scripts/slmatchups/eight_ball.py
@@ -87,60 +87,60 @@ def get_sl_matchup_stats_eight(df, games2win):
     slmatches_average.to_excel(r'../../data/SLMatchupAverages.xlsx', index=True, header=True)
     print(slmatches_average)
     # get median and mode
-    slmatches_median = games2win
-    slmatches_mode = games2win
-    for p1skill in slrange:
-        for p2skill in slrange:
-            matchup_data = SLMatchesEight(p1skill, p2skill)
-            for index, row in df.iterrows():
-                if row['Player_1'] == p1skill and row['Player_2'] == p2skill:
-                    if row['Points_1'] == 0 and row['Points_2'] == 3:
-                        matchup_data.resultcounts["0/3"] += 1
-                    elif row['Points_1'] == 0 and row['Points_2'] == 2:
-                        matchup_data.resultcounts["0/2"] += 1
-                    elif row['Points_1'] == 1 and row['Points_2'] == 2:
-                        matchup_data.resultcounts["1/2"] += 1
-                    elif row['Points_1'] == 2 and row['Points_2'] == 1:
-                        matchup_data.resultcounts["2/1"] += 1
-                    elif row['Points_1'] == 2 and row['Points_2'] == 0:
-                        matchup_data.resultcounts["2/0"] += 1
-                    elif row['Points_1'] == 3 and row['Points_2'] == 0:
-                        matchup_data.resultcounts["3/0"] += 1
-                    matchup_data.addgame(index)
-            for index, row in df.iterrows():
-                if index not in matchup_data.indices_checked:
-                    if row['Player_2'] == p1skill and row['Player_1'] == p2skill:
-                        if row['Points_2'] == 0 and row['Points_1'] == 3:
-                            matchup_data.resultcounts["0/3"] += 1
-                        elif row['Points_2'] == 0 and row['Points_1'] == 2:
-                            matchup_data.resultcounts["0/2"] += 1
-                        elif row['Points_2'] == 1 and row['Points_1'] == 2:
-                            matchup_data.resultcounts["1/2"] += 1
-                        elif row['Points_2'] == 2 and row['Points_1'] == 1:
-                            matchup_data.resultcounts["2/1"] += 1
-                        elif row['Points_2'] == 2 and row['Points_1'] == 0:
-                            matchup_data.resultcounts["2/0"] += 1
-                        elif row['Points_2'] == 3 and row['Points_1'] == 0:
-                            matchup_data.resultcounts["3/0"] += 1
-                        matchup_data.addgame(index)
-            matchup_data.getmedian()
-            try:
-                slmatches_median.loc[p1skill, p2skill] = matchup_data.median
-            except TypeError:
-                slmatches_median.loc[p1skill, p2skill] = matchup_data.median
-            matchup_data.getmode()
-            try:
-                slmatches_mode.loc[p1skill, p2skill] = matchup_data.mode
-            except TypeError:
-                slmatches_mode.loc[p1skill, p2skill] = matchup_data.mode
-    sls = pd.DataFrame(slmatches_median, index=slrange, columns=slrange, dtype=str)
-    dfi.export(sls, "../../data/images/slMatchupMedians.png")
-    slmatches_median.to_excel(r'../../data/SLMatchupMedians.xlsx', index=True, header=True)
-    print(slmatches_median)
-    sls = pd.DataFrame(slmatches_mode, index=slrange, columns=slrange, dtype=str)
-    dfi.export(sls, "../../data/images/slMatchupModes.png")
-    slmatches_mode.to_excel(r'../../data/SLMatchupModes.xlsx', index=True, header=True)
-    print(slmatches_mode)
+    # slmatches_median = games2win
+    # slmatches_mode = games2win
+    # for p1skill in slrange:
+    #     for p2skill in slrange:
+    #         matchup_data = SLMatchesEight(p1skill, p2skill)
+    #         for index, row in df.iterrows():
+    #             if row['Player_1'] == p1skill and row['Player_2'] == p2skill:
+    #                 if row['Points_1'] == 0 and row['Points_2'] == 3:
+    #                     matchup_data.resultcounts["0/3"] += 1
+    #                 elif row['Points_1'] == 0 and row['Points_2'] == 2:
+    #                     matchup_data.resultcounts["0/2"] += 1
+    #                 elif row['Points_1'] == 1 and row['Points_2'] == 2:
+    #                     matchup_data.resultcounts["1/2"] += 1
+    #                 elif row['Points_1'] == 2 and row['Points_2'] == 1:
+    #                     matchup_data.resultcounts["2/1"] += 1
+    #                 elif row['Points_1'] == 2 and row['Points_2'] == 0:
+    #                     matchup_data.resultcounts["2/0"] += 1
+    #                 elif row['Points_1'] == 3 and row['Points_2'] == 0:
+    #                     matchup_data.resultcounts["3/0"] += 1
+    #                 matchup_data.addgame(index)
+    #         for index, row in df.iterrows():
+    #             if index not in matchup_data.indices_checked:
+    #                 if row['Player_2'] == p1skill and row['Player_1'] == p2skill:
+    #                     if row['Points_2'] == 0 and row['Points_1'] == 3:
+    #                         matchup_data.resultcounts["0/3"] += 1
+    #                     elif row['Points_2'] == 0 and row['Points_1'] == 2:
+    #                         matchup_data.resultcounts["0/2"] += 1
+    #                     elif row['Points_2'] == 1 and row['Points_1'] == 2:
+    #                         matchup_data.resultcounts["1/2"] += 1
+    #                     elif row['Points_2'] == 2 and row['Points_1'] == 1:
+    #                         matchup_data.resultcounts["2/1"] += 1
+    #                     elif row['Points_2'] == 2 and row['Points_1'] == 0:
+    #                         matchup_data.resultcounts["2/0"] += 1
+    #                     elif row['Points_2'] == 3 and row['Points_1'] == 0:
+    #                         matchup_data.resultcounts["3/0"] += 1
+    #                     matchup_data.addgame(index)
+    #         matchup_data.getmedian()
+    #         try:
+    #             slmatches_median.loc[p1skill, p2skill] = matchup_data.median
+    #         except TypeError:
+    #             slmatches_median.loc[p1skill, p2skill] = matchup_data.median
+    #         matchup_data.getmode()
+    #         try:
+    #             slmatches_mode.loc[p1skill, p2skill] = matchup_data.mode
+    #         except TypeError:
+    #             slmatches_mode.loc[p1skill, p2skill] = matchup_data.mode
+    # sls = pd.DataFrame(slmatches_median, index=slrange, columns=slrange, dtype=str)
+    # dfi.export(sls, "../../data/images/slMatchupMedians.png")
+    # slmatches_median.to_excel(r'../../data/SLMatchupMedians.xlsx', index=True, header=True)
+    # print(slmatches_median)
+    # sls = pd.DataFrame(slmatches_mode, index=slrange, columns=slrange, dtype=str)
+    # dfi.export(sls, "../../data/images/slMatchupModes.png")
+    # slmatches_mode.to_excel(r'../../data/SLMatchupModes.xlsx', index=True, header=True)
+    # print(slmatches_mode)
     plt.clf()
 
     # for sl in slrange:
