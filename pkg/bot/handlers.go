dc81fb7bf7e96b003e93a921bf831925bc5078b9
Scott Berry <berryscottr@gmail.com>
2023-06-21 13:12:41 -0400

    clean strings

--- pkg/bot/handlers.go
+++ pkg/bot/handlers.go
@@ -194,7 +194,7 @@ func (bot *Data) HandleGameDay(s *discordgo.Session, m *discordgo.MessageCreate,
 	}
 	var opponentTeam string
 	for i, name := range team.DivisionTeamNames {
-		for _, junk := range []string{"'", "-", "8", "9"} {
+		for _, junk := range []string{"'", "-", "8", "9", " - "} {
 			name = strings.Replace(name, junk, "", -1)
 		}
 		if strings.Contains(strings.ToLower(m.Content), strings.ToLower(name)) {
